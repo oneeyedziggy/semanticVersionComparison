6297e55a6b0494558a8fc52d43214008c2be1ea7
oneeyedziggy <oneeyedziggy@gmail.com>
2018-06-27 13:00:53 -0700

    Update verComp.sh
    modified to use leftPad.sh, which should posted immediately after this

--- verComp.sh
+++ verComp.sh
@@ -1,30 +1,46 @@
 #!/usr/bin/env bash
-if [ $1 = 'h' ] || [ $1 = '-h' ]; then
+
+if [[ $1 = 'h' ]] || [[ $1 = '-h' ]]; then
   echo 'This utility will accept two semantic version numbers w/ parts less than 1000000 each, and return \'greater\', \'less\', or \'equal\'.'';
   exit;
 fi
+
 normalizeVersion () {
   #split into parts
-  inputBuffer=$(echo "$1" | tr "." "\\n" );
+  inputBuffer=($(echo $1 | tr "." " "));
+
   inputVersion="";
+  
   #remove leading 0s
-  for verPart in $inputBuffer
-  do
-    printf -v verPart "%06s" $verPart
-    inputVersion+=$verPart;
+  for verPart in ${inputBuffer[@]}; do
+
+    #printf -v verPart "%06s" $verPart
+
+    #./leftPad stringToPad padToLen [ padChar, default 0 ]
+    verPartBuffer=$( ./leftPad.sh ${verPart} 6 )
+
+    inputVersion+=${verPartBuffer};
   done;
+
   #remove leading 0s
   echo $inputVersion;
 }
+
 comparedValue=$(normalizeVersion $1)
+
+#echo "$1="$1;
+#echo "comparedValue="${comparedValue};
+
 compareTo=$(normalizeVersion $2)
-#echo "$comparedValue";
-#echo "$compareTo";
-if [ $comparedValue -eq $compareTo ]; then
+#echo "$2="$2;
+#echo "compareTo="${compareTo};
+
+if [ ${comparedValue} -eq ${compareTo} ]; then
   echo 'equal'
-elif [ $comparedValue -gt $compareTo ]; then
+elif [ ${comparedValue} -gt ${compareTo} ]; then
   echo 'greater'
-elif [ $comparedValue -lt $compareTo ]; then
+elif [ ${comparedValue} -lt ${compareTo} ]; then
   echo 'less'
 fi
+
 exit;
